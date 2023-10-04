a111fb723b875f565d4dcabc3103718f822cbd79
Juhana Kaarlehto <juhana.kaa@gmail.com>
2023-10-04 20:58:02 +0300

    exercise 1.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,5 @@
+//Exercises 1.1 and 1.2 in separate commits
+//
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -7,21 +9,40 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+     	<Header course={course} />
+	  //God awful
+	<Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+	  //Should pass props as an array and sum them up in the component.
+	<Total first={exercises1} second={exercises2} third={exercises3}/>
     </div>
   )
 }
 
+const Header = ({course}) => {
+
+	return(
+		<>
+			<h1>{course}</h1>
+		</>
+	);
+}
+
+const Content = (props) => {
+	return(
+		<>
+	<p> {props.part1} {props.exercises1} </p>
+	<p> {props.part2} {props.exercises2} </p>
+	<p> {props.part3} {props.exercises3} </p>
+
+		</>
+	);
+}
+const Total = (props) => {
+	return(
+		<p> Number of exercises {props.first + props.second + props.third} </p>
+	);
+}
 export default App
