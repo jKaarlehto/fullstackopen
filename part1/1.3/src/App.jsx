6096972acdad2fb21ed6d71bcb3f651c32935b3f
Juhana Kaarlehto <juhana.kaa@gmail.com>
2024-12-11 20:40:27 +0200

    1.4

--- part1/1.3/src/App.jsx
+++ part1/1.3/src/App.jsx
@@ -1,29 +1,32 @@
 //Exercises in separate commits
 //
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+    const course = 'Half Stack application development'
 
-  return (
-    <div>
-	<Header course={course}/> 
-	<Content part={part1}/>
-	<Content part={part2}/>
-	<Content part={part3}/>
-	<Total part1={part1} part2={part2} part3={part3}/>
-    </div>
-  )
+
+
+    const parts = [
+        {
+        name: 'Fundamentals of React',
+        exercises: 10
+        },
+        {
+        name: 'Using props to pass data',
+        exercises: 7
+        },
+        {
+        name: 'State of a component',
+        exercises: 14
+        }
+    ]
+        return (
+            <div>
+            <Header course={course}/> 
+            <Content parts={parts}/>
+            <Total parts={parts}/>
+            </div>
+        )
+    
 }
 
 
@@ -36,21 +39,19 @@ const Header = ({course}) => {
 	);
 }
 
-const Content = ({part}) => {
-	return(
-		<>
-			<p>{part["name"]} {part["exercises"]}</p>
-		</>
-	);
+const Content = ({parts}) => {
+  const content = parts.map(part => <p>  {part.name} {part.exercises} </p>)
+      return(
+        <>
+          {content}
+        </>
+      );
 }
 
-const Total = (props) => {
+const Total = ({parts}) => {
 	
-	{/*Should take props as an array and sum them up in the component.*/}
-
-	console.log(props)
 	return(
-		<p> Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises} </p>
+		<p> Number of exercises {parts[0].exercises+parts[1].exercises+parts[2].exercises} </p>
 	);
 }
 
