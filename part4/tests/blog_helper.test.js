ec9688074d2329b905765955dbd7dda25cdf77d7
Juhana Kaarlehto <juhana.kaa@gmail.com>
2025-01-29 19:59:39 +0200

    4.5

--- part4/tests/blog_helper.test.js
+++ part4/tests/blog_helper.test.js
@@ -1,24 +1,39 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
+const testVars = require('./test_vars')
 const blogHelper = require('../utils/blog_helper')
-const { on } = require('node:events')
 
-test.describe('total likes', () => {
-	const listWithOneBlog = [
-		{
-			_id: '5a422aa71b54a676234d17f8',
-			title: 'Go To Statement Considered Harmful',
-			author: 'Edsger W. Dijkstra',
-			url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-			likes: 5,
-			__v: 0
-		}
-	]
+
+test('dummy returns one', () => {
+  const blogs = []
+  const result = blogHelper.dummy(blogs)
+  assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
 	
-	test('when list only contains one blog b ==> total likes = b.likes'), () => {
-	    const result = blogHelper.totalLikes(listWithOneBlog)
+    test('when list only contains one blog b ==> total likes = b.likes', () => {
+	    const result = blogHelper.totalLikes(testVars.oneBlogArr)
 	    assert.strictEqual(result,5)
-	}
+	})
+
+
+})
+describe('favorite blog', () => {
+    
+    test('is the blog with most likes', () => {
+	const result = blogHelper.favoriteBlog(testVars.manyBlogsArr)
+	answer = 
+	    {
+		_id: "5a422b3a1b54a676234d17f9",
+		title: "Canonical string reduction",
+		author: "Edsger W. Dijkstra",
+		url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+		likes: 12,
+		__v: 0
+	    }
+	assert.deepStrictEqual(result,answer)
+    })
 
 })
 
--- part4/utils/blog_helper.js
+++ part4/utils/blog_helper.js
@@ -1,11 +1,21 @@
 Blog = require('../models/blog')
 
+const dummy = (dummy) => 1
+
 const totalLikes = (blogs) => {
-  return blogs.reduce((sum, blog) => {
-	let likes = Number(blog.likes)
-	sum += likes
-    },0)
+	return blogs.reduce((sum, blog) => {
+		let likes = Number(blog.likes)
+		return sum + likes
+	}, 0)
 }
 
-module.exports = {totalLikes}
-    
+const favoriteBlog = (blogs) => {
+	if (blogs.length === 0) return null
+	return blogs.reduce((highest, blog) => {
+		if (isNaN(blog.likes)) throw new Error(`likes should be a number. ${JSON.stringify(blog)}`);
+		return blog.likes > highest.likes ? blog : highest;
+	}, blogs[0]);
+}
+
+module.exports = { totalLikes, dummy, favoriteBlog }
+
