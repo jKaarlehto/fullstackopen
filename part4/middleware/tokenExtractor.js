4d93dea143b37d88aeccfe914265b9bc482f734c
Juhana Kaarlehto <juhana.kaa@gmail.com>
2025-02-04 14:27:33 +0200

    5.1

--- part4/middleware/tokenExtractor.js
+++ part4/middleware/tokenExtractor.js
@@ -3,6 +3,7 @@ const tokenExtractor = (request, response, next) => {
 
     const authorization = request.get('authorization')
     if (authorization && authorization.startsWith('Bearer ')) {
+	    
 	request.token = authorization.replace('Bearer ', '')
     }
     next() 
---
+++ part5/src/App.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+	const [blogs, setBlogs] = useState([])
+	const [username, setUsername] = useState([])
+	const [password, setPassword] = useState([])
+	const [user, setUser] = useState(null)
+	const [errorMessage, setErrorMessage] = useState(null)
+
+	useEffect(() => {
+		blogService.getAll().then(blogs =>
+			setBlogs(blogs)
+		)
+	}, [])
+
+	const handleLogin = async (event) => {
+		event.preventDefault()
+		console.log('logging in with', username, password)
+		try {
+			const user = await loginService.login({ username, password })
+			setUser(user)
+			setUsername('')
+			setPassword('')
+		} catch (error) {
+			setErrorMessage('wrong credentials')
+			setTimeout(() => {
+				setErrorMessage(null)
+			}, 5000)
+		}
+	}
+
+	const loginForm = () => {
+	    return (
+	    <div>
+		<h2>Login</h2>
+			  <h3>{errorMessage}</h3>
+			  <form onSubmit={handleLogin}>
+			    <div>
+			      username
+				<input
+				type="text"
+				value={username}
+				name="Username"
+				onChange={({ target }) => setUsername(target.value)}
+			      />
+			    </div>
+			    <div>
+			      password
+				<input
+				type="password"
+				value={password}
+				name="Password"
+				onChange={({ target }) => setPassword(target.value)}
+			      />
+			    </div>
+			    <button type="submit">login</button>
+			  </form>
+	    </div>
+	    )
+	}
+
+	const blogsForm = () => {
+	    return (
+		<div>	    
+		    <h2>blogs</h2>
+		    {blogs.map(blog =>
+			    <Blog key={blog.id} blog={blog} />
+		    )}
+		</div>
+	    )
+	}
+
+	return (
+	    <div>
+	    <p>{user && `Logged in as ${user.name}`}</p>
+	    {!user && loginForm()}
+	    {user && blogsForm()}
+	    </div>
+		)
+	
+}
+
+	export default App
